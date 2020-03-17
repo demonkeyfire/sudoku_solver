a891554345eb466267865f9744c67f5614daece1
demonkeyfire <adamsbrown.work@gmail.com>
2020-03-17 21:23:51 +0100

    Replaced dictionaries with numpy array reference - Originally read the sudoku puzzle in as numpy array and separated the rows, columns and 3x3 arrays into dictionaries - Instead use the numpy array directly which simplifies replacing values - def single_number() now gives a simple description of the change to the array - Use '__main__' to run the calculations

--- solver.py
+++ solver.py
@@ -2,56 +2,78 @@
 import numpy as np
 
 
-# Reading in sudoku array
-sudoku_in = np.loadtxt("input.txt",dtype="int") # Reading in sudoku text file as numpy array
-sudoku = sudoku_in # Backing up input sudoku
-default = np.array([1,2,3,4,5,6,7,8,9]) # 1D array of sudoku numbers
-
-
-# ------------------------DEFINING THE ROWS AND COLUMNS OF THE SUDOKU PUZZLE-----------------------
-
-# A dictionary of the rows
-rows = {}
-for i in range(0,9): # cycling through all rows
-    rows["row_" + str(i)] = sudoku_in[i,:] # picking up each row
-
-# A dictionary of the columns
-columns = {}
-for i in range(0,9):
-    columns["column_" + str(i)] = sudoku_in[:,i]
-
-# Creating a dictionary of the slices
-slices = {}
-for i in range(0,3):
-    for j in range(0,3):
-        slices["slice_"+str(i)+"_"+str(j)] = sudoku_in[j*3:(j*3)+3,i*3:(i*3)+3]
-
-
 # --------------------------------SOLVING FOR A SINGLE MISSING VALUE-------------------------------
-
-def single_number():
-    k = 0
-    while (np.any(sudoku==0))==True: # Checking if the sudoku is solved
-        k += 1
-        if k > 10:
-            exit()
-        else:
-            # Searching rows for single missing values
-            if (np.sum(np.isin(rows["row_" + str(i)],default))) == 8: # checking if a single value is missing
-                number = 45 - np.sum(rows["row_" + str(i)]) # calculating missing value
-                location = np.argwhere(rows["row_" + str(i)]==0) # location of missing value
-                rows["row_" + str(i)] = np.where(rows["row_" + str(i)]==0, number, rows["row_" + str(i)]) # replacing missing value
-
+def single_number(sudoku, default, k):
+    # Searching rows for single missing values
+    if k==0:
+        for i in range(0,9):
+            if (np.sum(np.isin(sudoku[i,:],default))) == 8: # checking if a single value is missing
+                print("The initial row "+str(i+1)+":")
+                print(sudoku[i,:])
+                number = 45 - np.sum(sudoku[i,:]) # calculating missing value
+                location = np.argwhere(sudoku[i,:]==0) # location of missing value
+                sudoku[i,:] = np.where(sudoku[i,:]==0, number, sudoku[i,:]) # replacing missing value
+                print("\nThe missing value at location "+str(np.asscalar(location)+1)+" is "+str(number))
+                print("\nThe final row "+str(i+1) + ":")
+                print(sudoku[i,:])
+            result = sudoku
+    elif k==1:
+        for i in range(0,9):
             # Searching columns for single missing values
-            if (np.sum(np.isin(columns["column_" + str(i)],default))) == 8:
-                number = 45 - np.sum(columns["column_" + str(i)])
-                location = np.argwhere(columns["column_" + str(i)]==0)
-                columns["column_" + str(i)] = np.where(columns["column_" + str(i)]==0, number, columns["column_" + str(i)])
-
+            if (np.sum(np.isin(sudoku[:,i],default))) == 8:
+                print("The initial column "+str(i+1) + ":")
+                print(sudoku[:,i])
+                number = 45 - np.sum(sudoku[:,i])
+                location = np.argwhere(sudoku[:,i]==0)
+                sudoku[:,i] = np.where(sudoku[:,i]==0, number, sudoku[:,i])
+                print("\nThe missing value at location "+str(np.asscalar(location)+1)+" is "+str(number))
+                print("\nThe final column "+str(i+1)+":")
+                print(sudoku[:,i])
+            result = sudoku
+    elif k==2:
+        for i in range(0,3):
+            for j in range(0,3):
             # Searching the 3x3 arrays for single solutions
-            if (np.sum(np.isin(slices["slice_"+str(i)+"_"+str(j)],default))) == 8:
-                number = 45 - np.sum(slices["slice_"+str(i)+"_"+str(j)])
-                location = np.argwhere(slices["slice_"+str(i)+"_"+str(j)]==0)
-                slices["slice_new_"+str(i)+"_"+str(j)] = np.where(slices["slice_"+str(i)+"_"+str(j)]==0, number, slices["slice_"+str(i)+"_"+str(j)])
+                if (np.sum(np.isin(sudoku[j*3:(j*3)+3,i*3:(i*3)+3],default))) == 8:
+                    print("The initial slice "+str(i+1)+","+str(j+1)+":")
+                    print(sudoku[j*3:(j*3)+3,i*3:(i*3)+3])
+                    number = 45 - np.sum(sudoku[j*3:(j*3)+3,i*3:(i*3)+3])
+                    location = np.argwhere(sudoku[j*3:(j*3)+3,i*3:(i*3)+3]==0)
+                    sudoku[j*3:(j*3)+3,i*3:(i*3)+3] = np.where(sudoku[j*3:(j*3)+3,i*3:(i*3)+3]==0, number, sudoku[j*3:(j*3)+3,i*3:(i*3)+3])
+                    print("\nThe missing value at location "+str(np.asscalar(location[0,0]))+","+str(np.asscalar(location[0,1]))+" is "+str(number))
+                    print("\nThe initial slice "+str(i+1)+","+str(j+1)+":")
+                    print(sudoku[j*3:(j*3)+3,i*3:(i*3)+3])
+                result = sudoku
+    return result
 
-                
+
+# ----------------------------------SOLVING FOR TWO MISSING VALUES---------------------------------
+def double_number(sudoku, default):
+    # Finding the 0's in a row
+    for i in range(0,9):
+        if i==0:
+            print("This is the row:")
+            print(sudoku[:,i])
+            for j in np.argwhere(sudoku[:,i]==0):
+                for k in sudoku[int(j),:]:
+                    if k==1:
+                        continue
+
+
+# -------------------------------------------MAIN PROGRAM------------------------------------------
+if __name__ == '__main__':
+
+    sudoku_in = np.loadtxt("input_2.txt", dtype="int") # Reading in sudoku puzzle
+    sudoku = sudoku_in # Backing up input sudoku to a working array
+    default = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]) # 1D array of sudoku numbers
+
+
+    # Replace single values in rows, columns and slices
+    '''count = 9
+    while (np.any(sudoku==0))==True:
+        count += 1
+        if count > 10:
+            exit()
+        for i in range(0,3):
+            single_number(sudoku, default, i)'''
+    double_number(sudoku, default)
